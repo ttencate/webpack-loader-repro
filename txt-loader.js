9151af7c9c33932f68b4298e730033da93e49380
Thomas ten Cate <ttencate@gmail.com>
2021-11-30 10:08:41 +0100

    Generate TypeScript that is not JavaScript

--- txt-loader.js
+++ txt-loader.js
@@ -1,4 +1,4 @@
 module.exports = function TxtLoader(txt) {
   console.log(`TxtLoader invoked with: ${txt}`)
-  return `export const TEXT = ${JSON.stringify(txt)}`
+  return `export const TEXT: string = ${JSON.stringify(txt)}`
 }
