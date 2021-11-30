d28260d9a8e76421c380f26243141d84f52179e9
Thomas ten Cate <ttencate@gmail.com>
2021-11-30 11:51:36 +0100

    Make loader die when given invalid input

--- txt-loader.js
+++ txt-loader.js
@@ -1,4 +1,7 @@
 module.exports = function TxtLoader(txt) {
-  console.log(`TxtLoader invoked with: ${txt}`)
+  console.log(`TxtLoader invoked on ${this.resourcePath} with content ${JSON.stringify(txt)}`)
+  if (txt.indexOf('Hello') < 0) {
+    throw new Error(`No "Hello" found`)
+  }
   return `export const TEXT: string = ${JSON.stringify(txt)}`
 }
